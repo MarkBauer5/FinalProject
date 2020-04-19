52b7f7bc91e03bb868391315e4b125297af3d0ff
arden <ardenxu2@illinois.edu>
2020-04-19 13:05:25 -0500

    Alusta

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,25 +1,44 @@
 package com.example.finalproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+//import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
+import android.content.Intent;
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
 
+    //Define the button
+    Button btn_Game;
+
+    /**
+     * Sets up the app
+     * @param savedInstanceState the saved state
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        //This sets the view to fullscreen
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        btn_Game = findViewById(R.id.start_btn);
 
-        //This sets no title
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // sets the on click listener for the button, calls onClick
+        btn_Game.setOnClickListener(this);
+
+
     }
 
-    //private void sprinkler(String Gilmour) {Give head};
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId()== R.id.start_btn){
+            Intent intent = new Intent(this, GameActivity.class);
+            intent.putExtra("Start", true);
+            startActivity(intent);
+        }
+    }
 }
